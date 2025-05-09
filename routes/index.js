f5b1ed7e04798ca2b40b7a3f459c829905e41633
Stephanie Dacullo <stephanie.laruga101@yahoo.com>
2025-05-09 02:26:02 -0400

    Adding mongodb and Get/GetAll endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-const router = require('express').Router();
+const router = require('express').Router(); 
 
 router.get('/', (req, res) => {res.send('Hello World');});
+
+router.use('/users', require('./users')); //gonna look for users.js file under routes
 
 module.exports = router;
